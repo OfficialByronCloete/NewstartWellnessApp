ab91c0f8994dba265164da381068889a2cb3460e
Byron Cloete <byroncloete7@gmail.com>
2026-04-12 23:27:51 +0200

    Updated Aspire orchestrator to run both FE + BE

--- backend/NewStartWellnessApp.Aspire/NewStartWellnessApp.Aspire.AppHost/AppHost.cs
+++ backend/NewStartWellnessApp.Aspire/NewStartWellnessApp.Aspire.AppHost/AppHost.cs
@@ -1,3 +1,11 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var api = builder.AddProject("webapi", @"..\..\NewStartWellnessApp.WebAPI\NewStartWellnessApp.WebAPI.csproj")
+    .WithExternalHttpEndpoints();
+
+builder.AddJavaScriptApp("frontend", @"..\..\..\frontend", runScriptName: "start")
+    .WithReference(api)
+    .WaitFor(api)
+    .WithHttpEndpoint(port: 4200, isProxied: false);
+
 builder.Build().Run();
